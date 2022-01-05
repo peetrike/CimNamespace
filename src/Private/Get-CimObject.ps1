eb379343b7b304e8b1f09197d3d3e91019142637
Peter Wawa <meelisn@outlook.com>
2022-01-05 18:39:53 +0200

    add helper functions

---
+++ src/Private/Get-CimObject.ps1
@@ -0,0 +1,31 @@
+﻿function Get-CimObject {
+    [CmdletBinding()]
+    param (
+            [parameter(
+                Mandatory = $true,
+                Position = 0
+            )]
+            [string]
+        $ClassName,
+            [string]
+        $Filter,
+            [string]
+        $Namespace
+    )
+
+    $CommandParams = @{}
+    if ($Namespace) {
+        $CommandParams.Namespace = $Namespace
+    }
+    if ($Filter) {
+        $CommandParams.Filter = $Filter
+    }
+
+    if ($Command = Get-Command Get-CimInstance -ErrorAction SilentlyContinue) {
+        $CommandParams.ClassName = $ClassName
+    } else {
+        $CommandParams.Class  = $ClassName
+        $Command = Get-Command Get-WmiObject
+    }
+    & $Command @CommandParams
+}
---
+++ src/Private/New-PSObject.ps1
@@ -0,0 +1,22 @@
+﻿function New-PSObject {
+    [CmdletBinding()]
+    param (
+            [string]
+        $TypeName,
+            [parameter(
+                Mandatory = $true,
+                Position = 0
+            )]
+            [Collections.IDictionary]
+        $Property
+    )
+
+    if ($PSVersionTable.PSVersion.Major -gt 2) {
+        if ($TypeName) { $Props.PSTypeName = $TypeName }
+        [pscustomobject] $Property
+    } else {
+        $object = New-Object -TypeName PSObject -Property $Property
+        if ($TypeName) { $object.psobject.TypeNames.Insert(0, $TypeName) }
+        $object
+    }
+}
