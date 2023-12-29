440c496bca01b185f39f89278f9e5d1bdcae994a
Peter Wawa <meelisn@outlook.com>
2023-12-29 17:47:41 +0200

    Add new private function

---
+++ src/Private/New-CimObject.ps1
@@ -0,0 +1,27 @@
+﻿function New-CimObject {
+    [CmdletBinding(
+        SupportsShouldProcess = $true
+    )]
+    param (
+            [Parameter(
+                Mandatory = $true,
+                Position = 0
+            )]
+            [string]
+        $ClassName,
+            [string]
+        $Namespace
+    )
+
+    $CommandParams = @{}
+    if ($Namespace) {
+        $CommandParams.Namespace = $Namespace
+    }
+
+    if (Get-Command New-CimInstance -ErrorAction SilentlyContinue) {
+        $CommandParams.ClassName = $ClassName
+        New-CimInstance @CommandParams -ClientOnly
+    } else {
+        (New-Object System.Management.ManagementClass($ClassName)).CreateInstance()
+    }
+}
