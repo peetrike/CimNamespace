0e363b28285ce4ee80675e1abe6dff039926148b
Peter Wawa <meelisn@outlook.com>
2025-01-16 14:20:35 +0200

    Remove all remoting parameters

--- src/Public/Get-CimNamespaceAccess.ps1
+++ src/Public/Get-CimNamespaceAccess.ps1
@@ -1,83 +1,36 @@
 ﻿function Get-CimNamespaceAccess {
     # .EXTERNALHELP CimNamespace-help.xml
-    [CmdletBinding(
-        DefaultParameterSetName = 'ComputerName'
-    )]
+    [CmdletBinding()]
     param (
             [Parameter(
-                Mandatory = $true,
-                Position = 0
+                Mandatory = $true
             )]
             [string]
-        $NameSpace,
-
-        #region Common parameters
-            [Parameter(
-                Mandatory = $true,
-                ParameterSetName = 'CimSession'
-            )]
-            #[CimSession[]]
-        $CimSession,
-            [Parameter(
-                ParameterSetName = 'ComputerName'
-            )]
-            [string]
-        $ComputerName,
-            [Parameter(
-                ParameterSetName = 'ComputerName'
-            )]
-            [Management.Automation.PSCredential]
-            [Management.Automation.Credential()]
-        $Credential #= [Management.Automation.PSCredential]::Empty
-        #endregion
+        $NameSpace
     )
 
-    begin {
-        $invokeParams = @{
-            ClassName   = '__SystemSecurity'
-            MethodName  = 'GetSecurityDescriptor'
-            ErrorAction = [Management.Automation.ActionPreference]::Stop
-        }
-
-        $sessionParams = @{}
-
-        switch ($PSCmdlet.ParameterSetName) {
-            'CimSession' {
-                $sessionParams.CimSession = $CimSession
-            }
-            Default {
-                if ($PSBoundParameters.ContainsKey('Credential')) {
-                    $sessionParams.Credential = $Credential
-                }
-                if ($ComputerName) {
-                        $sessionParams.ComputerName = $ComputerName
-                }
-            }
-        }
+    $invokeParams = @{
+        ClassName  = '__SystemSecurity'
+        MethodName = 'GetSecurityDescriptor'
+        Namespace  = $NameSpace
     }
 
-    process {
-        $invokeParams.Namespace = $NameSpace
+    $output = Use-CimMethod @invokeParams -ErrorAction Stop
 
-        $output = Use-CimMethod @invokeParams @sessionParams
+    if ($output.ReturnValue -ne 0) {
+        throw ('GetSecurityDescriptor failed: {0}' -f $output.ReturnValue)
+    }
 
-        if ($output.ReturnValue -ne 0) {
-            throw ('GetSecurityDescriptor failed: {0}' -f $output.ReturnValue)
+    foreach ($ace in $output.Descriptor.DACL) {
+        $UserProps = @{
+            AccountName = '{0}\{1}' -f $ace.Trustee.Domain, $ace.Trustee.Name
+            SID         = [Security.Principal.SecurityIdentifier] $ace.Trustee.SidString
+            Type        = [Security.AccessControl.AceType] $ace.AceType
+            AceFlags    = [Security.AccessControl.AceFlags] $ace.AceFlags
+            IsInherited = ($ace.AceFlags -band [Security.AccessControl.AceFlags]::Inherited) -gt 0
+            Permission  = [CimNamespace.Dacl.AccessMask] $ace.AccessMask
         }
 
-        foreach ($ace in $output.Descriptor.DACL) {
-            $UserProps = @{
-                #Domain      = $ace.Trustee.Domain
-                #Name        = $ace.Trustee.Name
-                AccountName = '{0}\{1}' -f $ace.Trustee.Domain, $ace.Trustee.Name
-                SID         = [Security.Principal.SecurityIdentifier] $ace.Trustee.SidString
-                Type        = [Security.AccessControl.AceType] $ace.AceType
-                AceFlags    = [Security.AccessControl.AceFlags] $ace.AceFlags
-                IsInherited = ($ace.AceFlags -band [Security.AccessControl.AceFlags]::Inherited) -gt 0
-                Permission  = [CimNamespace.Dacl.AccessMask] $ace.AccessMask
-            }
-
-            New-PSObject -TypeName 'CimNamespace.ACE' -Property $UserProps
-        }
+        New-PSObject -TypeName 'CimNamespace.ACE' -Property $UserProps
     }
 }
